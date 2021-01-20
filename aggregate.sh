afe563aed991701390a21edb5bbd0613c1dee1ad
akritiko <akritiko@gmail.com>
2021-01-20 21:26:30 +0200

    5 new datasets added. Aggregation script updated. 2020-01-20 update

--- aggregate.sh
+++ aggregate.sh
@@ -26,13 +26,17 @@ cd ../
 cp -R covid19-Greece/* data/csv/
 rm -Rf covid19-Greece/
 
+# Download iMEdD-Lab data from Github.
+wget https://raw.githubusercontent.com/iMEdD-Lab/open-data/master/COVID-19/greeceTimeline.csv
+wget https://raw.githubusercontent.com/iMEdD-Lab/open-data/master/COVID-19/greece_cases_v2.csv
+wget https://raw.githubusercontent.com/iMEdD-Lab/open-data/master/COVID-19/greece_deaths_v2.csv
+wget https://raw.githubusercontent.com/iMEdD-Lab/open-data/master/COVID-19/schools.csv
 
 # Download Oxford Covid-19 Government Response Tracker data from Github.
 # 1. Opening Risk Index
 # 2. Government Measures Stringency Index
 wget https://github.com/OxCGRT/covid-policy-scratchpad/raw/master/risk_of_openness_index/data/riskindex_timeseries_latest.csv
 wget https://github.com/OxCGRT/covid-policy-tracker/raw/master/data/OxCGRT_latest.csv
-
 
 # Download mobility data.
 # 1. Google
@@ -52,22 +56,29 @@ wget https://github.com/ActiveConclusion/COVID19_mobility/raw/master/apple_repor
 # TomTom
 wget https://github.com/ActiveConclusion/COVID19_mobility/raw/master/tomtom_reports/tomtom_trafic_index.csv
 
-
 # Download vaccination data (worldwide) from Our World in Data (OWiD)
 wget https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv
-
 
 # Move rest of the downloads to data
 cp riskindex_timeseries_latest.csv data/csv/
 cp OxCGRT_latest.csv data/csv/
 cp tomtom_trafic_index.csv data/csv/
 cp apple_mobility_report.csv data/csv/
-cp vaccinations.csv data/csv/vaccinations.csv
+cp vaccinations.csv data/csv/
+cp greeceTimeline.csv data/csv/
+cp greece_cases_v2.csv data/csv/
+cp greece_deaths_v2.csv data/csv/
+cp schools.csv data/csv/
+
 # Clean up!
 rm riskindex_timeseries_latest.csv
 rm OxCGRT_latest.csv
 rm tomtom_trafic_index.csv
 rm apple_mobility_report.csv
 rm vaccinations.csv
+rm greeceTimeline.csv
+rm greece_cases_v2.csv 
+rm greece_deaths_v2.csv 
+rm schools.csv
 
 exit 0
